ab6b92c7e71fa4ce5e8cad978f3a6a12452e1f1e
Alexander Gugel <alexander.gugel@gmail.com>
2014-08-17 03:35:21 -0700

    Integrate segment.io

--- client/app.js
+++ client/app.js
@@ -1,19 +1,34 @@
 angular.module('app', [])
 
 .run(['$rootScope', function($rootScope) {
-  $rootScope.scrollTo = function(id) {
+  $rootScope.jumpTo = function(id) {
+    analytics.track('Jumped to section', {
+      section: id
+    });
     $('html, body').animate({
       scrollTop: $('#' + id).offset().top
     }, 100);
   };
+  $rootScope.track = function() {
+    return analytics.track.apply(analytics.track, arguments);
+  };
 }])
 
-.controller('CouponsController', ['$scope', '$http', '$anchorScroll', '$timeout', function($scope, $http) {
+.controller('CouponsController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
   $http({method: 'GET', url: 'api/coupons'}).success(function(data) {
     $scope.coupons = data;
   });
 
   $scope.filterByCategory = function(category) {
+    analytics.track('Filtered by category', {
+      category: category
+    });
     $scope.query = category;
+  };
+
+  $scope.trackFilterByQuery = function() {
+    analytics.track('Filtered by query', {
+      query: $scope.query
+    });
   };
 }]);
