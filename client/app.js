c010ba3f72a6f4a35fa097ffc66cb8ecd931eb9b
Alexander Gugel <alexander.gugel@gmail.com>
2014-08-16 20:13:05 -0700

    Fix anchor link

--- client/app.js
+++ client/app.js
@@ -1,25 +1,15 @@
-$(function() {
-  // Smooth scrolling
-  // http://css-tricks.com/snippets/jquery/smooth-scrolling/
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
 angular.module('app', [])
 
-.controller('CouponsController', ['$scope', '$http', function($scope, $http) {
+.run(['$rootScope', function($rootScope) {
+  $rootScope.scrollTo = function(id) {
+    $('html, body').animate({
+      scrollTop: $('#' + id).offset().top
+    }, 100);
+  };
+}])
+
+.controller('CouponsController', ['$scope', '$http', '$anchorScroll', '$timeout', function($scope, $http) {
   $http({method: 'GET', url: 'api/coupons'}).success(function(data) {
-    console.log(data);
     $scope.coupons = data;
   });
 
