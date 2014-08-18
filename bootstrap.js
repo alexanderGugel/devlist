15c7ac0b1b15c2b7b7bdebf4bd114f187b29de97
Alexander Gugel <alexander.gugel@gmail.com>
2014-08-17 17:44:54 -0700

    Randomize bootstrap script

--- bootstrap.js
+++ bootstrap.js
@@ -1,13 +1,15 @@
 var redis = require('./redis');
-var coupons = [{
-  company: 'New Relic',
-  description: '25% Off Your Entire Order Of Regular-Priced Items',
-  url: 'http://newrelic.com/',
-  favicon: 'http://newrelic.com/favicon.ico',
-  createdAt: new Date(),
-  code: 'biuref',
-  category: 'analytics'
-}];
+var genFakeCoupon = function() {
+  return {
+    company: Math.floor(Math.random()*100) + ' New Relic',
+    description: Math.floor(Math.random()*100) + '% Off Your Entire Order Of Regular-Priced Items',
+    url: 'http://newrelic.com/' + Math.floor(Math.random()*100),
+    favicon: 'http://newrelic.com/favicon.ico',
+    createdAt: new Date(),
+    code: 'biuref ' + Math.floor(Math.random()*100),
+    category: 'analytics ' + Math.floor(Math.random()*100)
+  };
+};
 
 var insertCoupon = function (coupon) {
   redis.incr('/coupons/counter', function (err, id) {
@@ -17,7 +19,5 @@ var insertCoupon = function (coupon) {
 };
 
 for (var j = 0; j < 100; j++) {
-  for (var i = 0; i < coupons.length; i++) {
-    insertCoupon(coupons[i]);
-  }
+  insertCoupon(genFakeCoupon());
 }
